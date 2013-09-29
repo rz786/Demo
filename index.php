6d0827fad2701a50dfc2094f47a588335ddf0b44
Rizwan Patel <rz786@gmail.com>
2013-09-29 21:31:09 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<title>Edu-C</title>
+
+	<link rel="stylesheet" href="css/foundation.css">
+	<link rel="shortcut icon" href="img/favicon.ico" >';
+	<script src="js/vendor/custom.modernizr.js"></script>
+</head>
+
+<body> 
+	<div class="row">
+		<div class="large-12 columns">
+
+		<!-- Navigation -->
+			<nav class="top-bar">
+				<ul class="title-area">
+					<!-- Title Area -->
+					<li class="name">
+						<h1><a href="#">Edu-C</a></h1>
+					</li>
+					<li class="toggle-topbar menu-icon"><a href="#"><span>menu</span></a></li>
+				</ul>
+
+				<section class="top-bar-section">
+					<ul class="left">
+						<li><a href="#">Courses</a></li>
+						<li><a href="#">Teach</a></li>
+						<li><a href="#">Register</a></li>
+					</ul>
+
+					<ul class="right">
+						<li>
+							<a class="button" href="#">Login</a>
+						</li>
+					</ul>
+				</section>
+			</nav>
+
+		<!-- End Navigation -->
+
+		</div>
+	</div>
+
+
+	<div class="row">
+		<div class="large-12 columns">
+
+		<!-- Desktop Slider -->
+
+			<div class="hide-for-small">
+				<div id="featured">
+					<img src="img/untitled.png" style="border:1px solid black;" alt="slide image">
+					<!--<img src="http://placehold.it/1000x400&text=Slide Image" alt="slide image">
+					<img src="http://placehold.it/1000x400&text=Slide Image" alt="slide image"> -->
+				</div>
+			</div>
+
+		<!-- End Desktop Slider -->
+
+
+		<!-- Mobile Header -->
+
+
+			<div class="row">
+				<div class="small-12 show-for-small"><br>
+					<img src="img/untitled.png" style="border:1px solid black;" />
+				</div>
+			</div>
+
+
+		<!-- End Mobile Header -->
+
+		</div>
+	</div><br>
+
+	<div class="row">
+		<div class="large-12 columns">
+			<div class="row">
+
+			<!-- Thumbnails -->
+
+				<div class="large-3 small-6 columns">
+					<a href="http://www.facebook.com" target="_blank" >
+						<img src="img/java.png" />
+						<h6 class="panel">Java SE 6.0</h6>
+					</a>
+				</div>
+
+				<div class="large-3 small-6 columns">
+					<img src="http://placehold.it/400x400&text=Thumbnail" />
+					<h6 class="panel">Description</h6>
+				</div>
+
+				<div class="large-3 small-6 columns">
+					<img src="http://placehold.it/400x400&text=Thumbnail" />
+					<h6 class="panel">Description</h6>
+				</div>
+
+				<div class="large-3 small-6 columns">
+					<img src="http://placehold.it/400x400&text=Thumbnail" />
+					<h6 class="panel">Description</h6>
+				</div>
+
+			<!-- End Thumbnails -->
+
+			</div>
+		</div>
+	</div>
+
+
+
+	<div class="row">
+		<div class="large-12 columns">
+			<div class="row">
+
+			<!-- Content -->
+
+			<div class="large-8 columns">
+				<div class="panel radius">
+
+				<div class="row">
+				<div class="large-6 small-6 columns">
+
+				<h4>Header</h4><hr/>
+				<h5 class="subheader">Risus ligula, aliquam nec fermentum vitae, sollicitudin eget urna. Donec dignissim nibh fermentum odio ornare sagittis.
+				</h5>
+
+				<div class="show-for-small" align="center">
+				<a href="#" class="small radius button">Call To Action!</a><br>
+
+				<a href="#" class="small radius button">Call To Action!</a>
+				</div>
+
+				</div>
+				<div class="large-6 small-6 columns">
+
+				<p>Suspendisse ultrices ornare tempor. Aenean eget ultricies libero. Phasellus non ipsum eros. Vivamus at dignissim massa. Aenean dolor libero, blandit quis interdum et, malesuada nec ligula. Nullam erat erat, eleifend sed pulvinar ac. Suspendisse ultrices ornare tempor. Aenean eget ultricies libero.
+				</p>
+				</div>
+
+				</div>
+				</div>
+			</div>
+
+			<div class="large-4 columns hide-for-small">
+
+			<h4>Get In Touch!</h4><hr/>
+
+			<a class="large button expand" href="#">
+			Call To Action!
+			</a>
+
+			<a class="large button expand" href="#">
+			Call To Action!
+			</a>
+
+
+			</div>
+
+			<!-- End Content -->
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Footer -->
+
+	<footer class="row">
+		<div class="large-12 columns">
+			<hr>
+			<div class="row">
+				<div class="large-6 columns">
+					<p>&copy; Copyright no one at all. Go to town.</p>
+				</div>
+				<div class="large-6 columns">
+					<ul class="inline-list right">
+						<li><a href="#">Link 1</a></li>
+						<li><a href="#">Link 2</a></li>
+						<li><a href="#">Link 3</a></li>
+						<li><a href="#">Link 4</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<!--to here, don't play with whatever's below-->
+
+	<script>
+	document.write('<script src=' +
+	('__proto__' in {} ? 'js/vendor/zepto' : 'js/vendor/jquery') +
+	'.js><\/script>')
+	</script>
+
+	<script src="js/foundation.min.js"></script>
+	<!--
+
+	<script src="js/foundation/foundation.js"></script>
+
+	<script src="js/foundation/foundation.alerts.js"></script>
+
+	<script src="js/foundation/foundation.clearing.js"></script>
+
+	<script src="js/foundation/foundation.cookie.js"></script>
+
+	<script src="js/foundation/foundation.dropdown.js"></script>
+
+	<script src="js/foundation/foundation.forms.js"></script>
+
+	<script src="js/foundation/foundation.joyride.js"></script>
+
+	<script src="js/foundation/foundation.magellan.js"></script>
+
+	<script src="js/foundation/foundation.orbit.js"></script>
+
+	<script src="js/foundation/foundation.reveal.js"></script>
+
+	<script src="js/foundation/foundation.section.js"></script>
+
+	<script src="js/foundation/foundation.tooltips.js"></script>
+
+	<script src="js/foundation/foundation.topbar.js"></script>
+
+	<script src="js/foundation/foundation.interchange.js"></script>
+
+	<script src="js/foundation/foundation.placeholder.js"></script>
+
+	<script src="js/foundation/foundation.abide.js"></script>
+
+	-->
+
+	<script>
+		$(document).foundation();
+	</script>
+</body>
+</html>
